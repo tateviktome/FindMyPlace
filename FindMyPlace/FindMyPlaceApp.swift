bdf9182c75fac01324c10145a746894f464c0dff
Tatevik <tatevik.tovmasyan@reaktor.com>
2023-01-10 17:02:40 +0400

    Initial Commit

---
+++ FindMyPlace/FindMyPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyPlaceApp.swift
+//  FindMyPlace
+//
+//  Created by Tatevik Tovmasyan on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FindMyPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
